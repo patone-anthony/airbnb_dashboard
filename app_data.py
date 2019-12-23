ca6c5c58d124c3f3a375a4e848bfc86f282b280d
Anthony Patone <patone.anthony@gmail.com>
2019-12-22 22:56:02 -0500

    added control panel

--- app_data.py
+++ app_data.py
@@ -37,7 +37,7 @@ app.layout = html.Div([
 
 
             html.H3(
-                "New York City Airbnb Open Data",
+                "New York City Airbnb Analytics Dashboard",
                 style={
                     "margin-bottom": "30px",
                     "margin-top": "30px",
@@ -52,7 +52,6 @@ app.layout = html.Div([
                 html.Div(['Filter the graphs using the selections below:'],
                          className="py-1 bold",
                          ),
-
 
                 html.Div(['Borough:']),
                 dcc.Dropdown(
@@ -88,10 +87,27 @@ app.layout = html.Div([
                         {'label': 'Shared room', 'value': 'Shared room'}
                     ],
                     value=['Private room', 'Entire home/apt', 'Shared room'],
+                    style={
+                        "margin-bottom": "15px",
+                    },
                 ),
 
+                html.Div(['Price:']),
+                dcc.RangeSlider(
+                    id="price_rangeslider",
+                    min=df['price'].min(),
+                    max=df['price'].max(),
+                    step=1,
+                    value=[(df['price'].min()), df['price'].max()],
+                    updatemode='drag',
+                ),
 
-
+                html.Div(
+                    id='output-price_rangeslider',
+                    style={
+                        "margin-left": "20px",
+                    },
+                )
 
             ], className='three columns pretty_container'),
 
@@ -99,10 +115,6 @@ app.layout = html.Div([
                 dcc.Graph(id='graph-all-apps'),
             ], className='nine columns pretty_container'),
         ], className='row flex-display'),
-
-
-
-
 
 
 
@@ -122,26 +134,47 @@ app.layout = html.Div([
 @app.callback(
     Output('graph-all-apps', 'figure'),
     [Input('date-picker-range', 'start_date'),
-     Input('date-picker-range', 'end_date')])
-def update_figure(start_date, end_date):
-
+     Input('date-picker-range', 'end_date'),
+     Input('price_rangeslider', 'value'),
+     Input('borough_dropdown', 'value'),
+     Input('room_checklist', 'value')])
+def update_figure(start_date, end_date, value, borough_values, room_values):
+    min_price = value[0]
+    max_price = value[1]
     filter_df = df[(df['last_review'] >= start_date) & (df['last_review'] <= end_date)]
+    filter_df = filter_df[(filter_df['price'] >= min_price) & (filter_df['price'] <= max_price)]
+
+    final_df = pd.DataFrame()
+
+    for borough_value in borough_values:
+        filter_df = df[df['neighbourhood_group'] == borough_value]
+        final_df = final_df.append(filter_df)
+
+    for room_value in room_values:
+        filter_df = df[df['neighbourhood_group'] == borough_value]
+        final_df = final_df.append(filter
+
 
     fig = go.Figure()
-    fig.add_trace(go.Histogram(histfunc="count", x=filter_df['neighbourhood_group']))
+    fig.add_trace(go.Histogram(histfunc="count", x=final_df['neighbourhood_group']))
     return fig
+
+
+
+
+
+
+@app.callback(
+    Output('output-price_rangeslider', 'children'),
+    [Input('price_rangeslider', 'value')])
+def update_output(value):
+    min_value = value[0]
+    max_value = value[1]
+    return f'Min Price: ${min_value}, Max Price: ${max_value}'
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
-
-
-
-
 
 
 
